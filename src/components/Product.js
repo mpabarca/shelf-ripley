a922b21cb6c1de507cb30cd053ca6f07727d209d
mpabarca <mpabarca@uc.cl>
2019-09-05 13:38:23 -0400

    button back on Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -1501,59 +1501,63 @@ class Product extends Component{
         });
 
         return(
-            <div className="product-container">
-                <div className="product-images">
-                    <ul className="side-images">
-                        {listImages}
-                    </ul>
-                    <div className="fullImage"> <img src={info.fullImage}/></div>
-                </div>
-
-                <div className="product-description">
-
-                    <div className="principal-description">
-                        <div className="product-brand">{(info.attributes)[0].value}</div>
-                        <div className="product-name">{info.name}</div>
-                        <div className="product-sku">SKU: {info.partNumber}</div>
-                        <div className="product-shortDescription">{info.shortDescription}</div>
+            <div>
+                <div className="back">volver</div>
+                <div className="product-container">
+                    <div className="product-images">
+                        <ul className="side-images">
+                            {listImages}
+                        </ul>
+                        <div className="fullImage"> <img src={info.fullImage}/></div>
                     </div>
 
-                    <div className="prices-product">
-                        <div className="normal-price">
-                            <div className="text">{((info.prices).formattedListPrice) ? (<h5>Normal</h5>) : ''}</div>
-                            <div className="price">{(info.prices).formattedListPrice}</div>
-                        </div>
-                        <div className="internet-price">
-                            <div className="text">{((info.prices).formattedOfferPrice) ? (<h5>Internet</h5>) : ''}</div>
-                            <div className="price">{(info.prices).formattedOfferPrice}</div>
-                        </div>
-                        <div className="card-price">
-                            <div className="text">{((info.prices).formattedCardPrice) ? (<h5>Tarjeta Ripley</h5>) : ''}</div>
-                            <div className="price">{(info.prices).formattedCardPrice}</div>
-                        </div>
-                        <div className="discount-price">
-                            <div className="text">{((info.prices).formattedCardPrice) ? (<h5>Descuento</h5>) : ''}</div>
-                            <div className="price">-{(info.prices).discountPercentage}%</div>
-                        </div>
-                    </div>
+                    <div className="product-description">
 
-                    <div className="second-description">
-                        <div className="color-product">
-                            <h6>COLOR</h6>
-                            <ul className="list-colors">
-                                {listColor}
-                            </ul>
+                        <div className="principal-description">
+                            <div className="product-brand">{(info.attributes)[0].value}</div>
+                            <div className="product-name">{info.name}</div>
+                            <div className="product-sku">SKU: {info.partNumber}</div>
+                            <div className="product-shortDescription">{info.shortDescription}</div>
                         </div>
 
-                        <div className="size-product">
-                            <h6>TALLA</h6>
-                            <ul className="list-sizes">
-                                {listSize}
-                            </ul>
+                        <div className="prices-product">
+                            <div className="normal-price">
+                                <div className="text">{((info.prices).formattedListPrice) ? (<h5>Normal</h5>) : ''}</div>
+                                <div className="price">{(info.prices).formattedListPrice}</div>
+                            </div>
+                            <div className="internet-price">
+                                <div className="text">{((info.prices).formattedOfferPrice) ? (<h5>Internet</h5>) : ''}</div>
+                                <div className="price">{(info.prices).formattedOfferPrice}</div>
+                            </div>
+                            <div className="card-price">
+                                <div className="text">{((info.prices).formattedCardPrice) ? (<h5>Tarjeta Ripley</h5>) : ''}</div>
+                                <div className="price">{(info.prices).formattedCardPrice}</div>
+                            </div>
+                            <div className="discount-price">
+                                <div className="text">{((info.prices).formattedCardPrice) ? (<h5>Descuento</h5>) : ''}</div>
+                                <div className="price">-{(info.prices).discountPercentage}%</div>
+                            </div>
+                        </div>
+
+                        <div className="second-description">
+                            <div className="color-product">
+                                <h6>COLOR</h6>
+                                <ul className="list-colors">
+                                    {listColor}
+                                </ul>
+                            </div>
+
+                            <div className="size-product">
+                                <h6>TALLA</h6>
+                                <ul className="list-sizes">
+                                    {listSize}
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            
         )
     }
 }
